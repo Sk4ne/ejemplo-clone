d43787b57c17dcdd3dce8758004b4ec577fff390
Spekth <neider86@protonmail.com>
2022-08-09 17:40:43 -0500

    Se cambio el código de respuesta 200 por 201 en el método post - Creación de usuarios

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -28,7 +28,7 @@ export const addQuestion = async (req: Request, res: Response, next: NextFunctio
   try {
     const body = req.body;
     const question = await Question.create(body);
-    res.status(200).json(question);
+    res.status(201).json(question);
   } catch (err) {
     res.status(500).json({
       message: `An error ocurred ${err}`
