b839887dbb2df3faf8dd17c39fcb6ee7142dd83c
Spekth <neider86@protonmail.com>
2022-08-11 12:39:26 -0500

    importamos Types de mongoose

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,12 +1,44 @@
-export {};
+/* export {};
 
 declare global {
   namespace Express {
     interface Request {
-      // user: any;
       user: {name:string,role:string};
     }
   }
 }
-type CookieSession = CookieSessionObject | null | undefined;  
-export { CookieSession }
+ */
+import { Types } from "mongoose";
+
+export interface UserReturnGoogle {
+  email:string;
+  given_name:string;
+  provider:string;
+} 
+
+export interface UserReturnFacebook {
+  first_name:string; 
+  email:string;
+} 
+
+export interface ProfileFacebook{
+  _json:{
+    id:string;
+    email:string;
+    first_name:string;
+    last_name?:string; 
+  }
+}
+
+export interface UserFacebook{
+  _id: string | Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  img: string;
+  role: string;
+  facebook: boolean;
+  google: boolean;
+  createAt: date,
+  state: boolean
+}
