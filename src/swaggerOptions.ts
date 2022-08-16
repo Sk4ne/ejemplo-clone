594967658fa7b17e54a0f375e263c29bf87e2498
Spekth <neider86@protonmail.com>
2022-08-16 17:33:22 -0500

    configuramos las opciones de swagger para documentar la API

---
+++ src/swaggerOptions.ts
@@ -0,0 +1,16 @@
+export const options = {
+  definition: {
+    openapi:"3.0.0",
+    info:{
+      title:'Survey API',
+      version:'1.0.0',
+      description:'Esta API llamada survey permite generan encuestas por medio de preguntas abierta y cerradas y a su vez descargarlas en formato pdf.'
+    },
+    servers:[
+      {
+        url:'http://localhost:3000/v1'
+      }
+    ]
+  },
+  apis:['./src/routes/v1/*.ts']
+}
