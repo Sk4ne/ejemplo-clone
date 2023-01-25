f83e6c39eb085db098ab6e7a10baa5102bdbb776
Spekth <neider86@protonmail.com>
2023-01-25 16:47:34 -0500

    Se modifico la ruta del server local por una ruta en la nube

--- src/swaggerDoc.ts
+++ src/swaggerDoc.ts
@@ -11,7 +11,8 @@ export const swaggerConfig = {
   },
   servers:[
     {
-      url:'http://localhost:3000/v1'
+      // url:'http://localhost:3000/v1'
+      url:'https://survey-production.up.railway.app/v1'
     }
   ],
   tags: [
