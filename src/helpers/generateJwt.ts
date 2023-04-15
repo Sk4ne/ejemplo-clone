577569ca828967f97c8cd26b568a5d9a20591025
Spekth <neider86@protonmail.com>
2023-04-15 15:40:59 -0500

    Se agrego el name y el email al payload

--- src/helpers/generateJwt.ts
+++ src/helpers/generateJwt.ts
@@ -3,9 +3,11 @@ dotenv.config();
 import jwt from 'jsonwebtoken';
 import { Types } from 'mongoose';
 
-export const generateJWT = (id:string | Types.ObjectId= '') => {
+// export const generateJWT = (id:string | Types.ObjectId= '') => {
+export const generateJWT = (id:string | Types.ObjectId= '', name?:string, email?: string) => {
    return new Promise((resolve,reject)=>{
-       const payload = { id };
+      //  const payload = { id };
+       const payload = { id,name,email };
        jwt.sign(payload,process.env.SECRET_OR_PRIVATE_KEY as string,{
          expiresIn: '10d'
        },(err,token)=>{
