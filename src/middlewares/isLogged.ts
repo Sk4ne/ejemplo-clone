b78dfe9ca3e3e854f9ecfb2e80028ae3ffcd24d1
Spekth <neider86@protonmail.com>
2023-06-24 12:55:39 -0500

    Se hizo una prueba para verificar si un usuario esta logueado con facebook

--- src/middlewares/isLogged.ts
+++ src/middlewares/isLogged.ts
@@ -1,11 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 
  export const isLoggedIn = async(req:Request,res:Response,next:NextFunction) => {
-  if (req.isAuthenticated()){
-    next();
+   // CODE ORIGINAL
+  /* if (req.isAuthenticated()){
+    // next();
+    // console.log(req.isAuthenticated())
+    
+    return next()
   }else{
     res.status(401).json({
       msg: 'Not Logged In'
     });
+  } */
+
+  if (req.user) {
+    next();
+  } else {
+    res.status(401).send('Not Logged In')
   }
 }
