8e101581ed819d463c2536d1d25279e38233263c
Spekth <neider86@protonmail.com>
2023-06-24 12:57:42 -0500

    Se elimino una interfaz que no se estaba usando

--- src/middlewares/validateRoles.ts
+++ src/middlewares/validateRoles.ts
@@ -3,12 +3,6 @@ import { Request,Response,NextFunction } from "express";
 
 
 export const userAdmin = (req: Request,res:Response,next:NextFunction) =>{
-
-  interface User {
-    role:string;
-    name:string; 
-  }
-  
   // const { role, name }:User = req.user;
   const roleUser:any = req.user;
   const nameUser:any = req.user;
